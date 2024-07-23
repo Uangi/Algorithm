d377db39553b2e8572d9990d1354261ef149ac24
박진영 <97930866+Uangi@users.noreply.github.com>
2024-07-23 22:38:48 +0900

    [level 1] Title: 평균 구하기, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        
+        double answer = 0;
+        int sum = 0;
+        int sol = arr.length;
+        for(int num : arr) {
+            sum += num;
+        }
+        answer = (double)sum / sol;
+        return answer;
+    }
+}
